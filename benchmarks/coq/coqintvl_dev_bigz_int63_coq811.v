25877acb2390a7263087214d3faa083de10cebcc
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2019-12-19 15:19:41 +0100

    Add examples suggested by Guillaume Melquiond

--- benchmarks/coq/coqintvl_dev_bigz_int63_coq811.v
+++ benchmarks/coq/coqintvl_dev_bigz_int63_coq811.v
@@ -975,3 +975,23 @@ Lemma example_ln_2 : (693/1000 < ln 2 < 694/1000)%R.
 idtac "example_ln_2".
 Time split; interval.
 Qed.
+
+(* From [Formally Verified Approx. of Definite Integrals by Mahboubi, Melquiond, Sibut-Pinote] *)
+
+Lemma bissect :
+  Rabs (RInt (fun x => x * sin x / (1 + cos x * cos x)) 0 PI - PI*PI/4) <= 1/1000000000000.
+idtac "bissect".
+Time integral with (i_degree 13, i_fuel 13, i_prec 53).
+Qed.
+
+Lemma Chebyshev :
+  (RInt (fun x => (2048 * x^12 - 6144 * x^10 + 6912 * x^8 - 3584 * x^6 + 840 * x^4 - 72 * x^2 + 1) * exp (-(x - 3/4)^2) * sqrt (1 - x*x)) (-1) 1 + 32555895745 / 10000000000000000) <= 1/100000000000.
+idtac "Chebyshev".
+Time integral with (i_degree 10, i_fuel 100, i_prec 53).
+Qed.
+
+Lemma Rump_Tucker :
+  Rabs (RInt (fun x => sin (x + exp x)) 0 8 - 3474/10000) <= 1/1000000.
+idtac "Rump_Tucker".
+Time integral with (i_degree 7, i_fuel 4000, i_prec 53).
+Qed.
